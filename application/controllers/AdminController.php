789438d5b539b165f4df45c80e1c68fbcbc5defa
Yaroslav <ray.last.light@gmail.com>
2021-09-26 16:39:15 +0300

    Create AdminController.php
    Create AdminController and create actions.

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace application\controllers;
+
+    use application\core\Controller;
+
+    class MainController extends Controller
+    {
+        public function loginAction()
+        {
+            $this->view->render('Вход');
+        }
+
+        public function logoutAction()
+        {
+            exit('Выход');
+        }
+
+        public function addAction()
+        {
+            $this->view->render('Добавить пост');
+        }
+
+        public function editAction()
+        {
+            $this->view->render('Редактировать');
+        }
+
+        public function deleteAction()
+        {
+            exit('Удаление');
+        }
+    }
