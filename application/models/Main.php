2493a2389d6ba0d553f6f473a9b5ffb31ce8f87d
Yaroslav <ray.last.light@gmail.com>
2021-10-01 18:39:24 +0300

    Update Main.php
    Add validate for form

--- application/models/Main.php
+++ application/models/Main.php
@@ -5,9 +5,28 @@
 
 class Main extends Model
     {
+        public $error;
         public function getNews()
         {
             $result = $this->db->row('SELECT title, description FROM news');
             return $result;
+        }
+
+        public function contactValidate($post)
+        {
+            $nameLen = iconv_strlen($post['name']);
+            $textLen = iconv_strlen($post['text']);
+            if($nameLen < 3 or $nameLen > 20){
+                $this->error = 'Имя должно содердать от 3 до 20 символов';
+                return false;
+            } elseif(!filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
+                $this->error = 'Неправильные email. Пример - bob@example.com';
+                return false;
+            } elseif($textLen < 5 or $textLen > 255){
+                $this->error = 'Поле должно содержать от 5 до 255 символов';
+                return false;
+            }
+
+            return true;
         }
     }
