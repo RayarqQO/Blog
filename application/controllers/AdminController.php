c71dd3db28024e29b1cd6e914c33394983f36951
Yaroslav <ray.last.light@gmail.com>
2021-10-03 00:40:15 +0300

    Update AdminController.php
    Login function for admin with validate. Also add logout

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -1,40 +1,58 @@
 <?php
 
-    namespace application\controllers;
+namespace application\controllers;
 
-    use application\core\Controller;
+use application\core\Controller;
 
-    class AdminController extends Controller
+class AdminController extends Controller
+{
+    public function __construct($route)
     {
-        public function __construct($route)
-        {
-            parent::__construct($route);
-            $this->view->layout = 'admin';
-            $_SESSION['admin'] = 0;
-        }
-
-        public function loginAction()
-        {
-            $this->view->render('Вход');
-        }
-
-        public function logoutAction()
-        {
-            exit('Выход');
-        }
-
-        public function addAction()
-        {
-            $this->view->render('Добавить пост');
-        }
-
-        public function editAction()
-        {
-            $this->view->render('Редактировать пост');
-        }
-
-        public function deleteAction()
-        {
-            exit('Удаление');
-        }
+        parent::__construct($route);
+        $this->view->layout = 'admin';
     }
+
+    public function loginAction()
+    {
+        if(isset($_SESSION['admin']))
+        {
+            $this->view->redirect('admin/add');
+        }
+        if(!empty($_POST))
+        {
+            if(!$this->model->loginValidate($_POST))
+            {
+                $this->view->message('erroe', $this->model->error);
+            }
+            $_SESSION['admin'] = true;
+            $this->view->location('admin/add');
+        }
+        $this->view->render('Вход');
+    }
+
+    public function logoutAction()
+    {
+        unset($_SESSION['admin']);
+        $this->view->redirect('admin/login');
+    }
+
+    public function addAction()
+    {
+        $this->view->render('Добавить пост');
+    }
+
+    public function editAction()
+    {
+        $this->view->render('Редактировать пост');
+    }
+
+    public function deleteAction()
+    {
+        exit('Удаление');
+    }
+
+    public function postsAction()
+    {
+        $this->view->render('Посты');
+    }
+}
