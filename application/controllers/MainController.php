54ac7e384f44a26f93f637574b60791ada658881
Yaroslav <ray.last.light@gmail.com>
2021-10-03 00:42:23 +0300

    Update MainController.php
    Contact action with form validation and sending message on email

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -1,36 +1,36 @@
 <?php
 
-    namespace application\controllers;
+namespace application\controllers;
 
-    use application\core\Controller;
+use application\core\Controller;
 
-    class MainController extends Controller
+class MainController extends Controller
+{
+    public function indexAction()
     {
-        public function indexAction()
-        {
-            $this->view->render('Главная страница');
-        }
-
-        public function aboutAction()
-        {
-            $this->view->render('Обо мне');
-        }
-
-        public function contactAction()
-        {
-            if(!empty($_POST)){
-                if(!$this->model->contactValidate($_POST))
-                {
-                    $this->view->message('erroe', $this->model->error);
-                }
-                mail('gm8i5@wimsg.com', 'Сообщение из блога', $_POST['name'].','.$_POST['email'].','.$_POST['text']);
-                $this->view->message('success', 'Сообщение отправлено администратору');
-            }
-            $this->view->render('Контакты');
-        }
-
-        public function postAction()
-        {
-            $this->view->render('Пост');
-        }
+        $this->view->render('Главная страница');
     }
+
+    public function aboutAction()
+    {
+        $this->view->render('Обо мне');
+    }
+
+    public function contactAction()
+    {
+        if(!empty($_POST)){
+            if(!$this->model->contactValidate($_POST))
+            {
+                $this->view->message('erroe', $this->model->error);
+            }
+            mail('gm8i5@wimsg.com', 'Сообщение из блога', $_POST['name'].','.$_POST['email'].','.$_POST['text']);
+            $this->view->message('success', 'Сообщение отправлено администратору');
+        }
+        $this->view->render('Контакты');
+    }
+
+    public function postAction()
+    {
+        $this->view->render('Пост');
+    }
+}
