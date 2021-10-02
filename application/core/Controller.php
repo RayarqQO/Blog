bd6f17e68c2fb82315b57144334df198f2b40620
Yaroslav <ray.last.light@gmail.com>
2021-10-03 00:42:47 +0300

    Update Controller.php
    Refactor

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -1,55 +1,55 @@
 <?php
 
-    namespace application\core;
+namespace application\core;
 
-    use application\core\View;
+use application\core\View;
 
-    abstract class Controller
+abstract class Controller
+{
+    public $route;
+    public $view;
+    public $acl;
+
+    public function __construct($route)
     {
-        public $route;
-        public $view;
-        public $acl;
+        $this->route = $route;
+        $this->view = new View($route);
+        $this->model = $this->loadModel($route['controller']);
+    }
 
-        public function __construct($route)
+    public function loadModel($name)
+    {
+        $path = 'application\models\\'.ucfirst($name);
+        if (class_exists($path)) 
         {
-            $this->route = $route;
-            $this->view = new View($route);
-            $this->model = $this->loadModel($route['controller']);
-        }
-
-        public function loadModel($name)
-        {
-            $path = 'application\models\\'.ucfirst($name);
-            if (class_exists($path)) 
-            {
-                return new $path;    
-            }
-        }
-
-        public function checkAcl()
-        {
-            $this->acl = require 'application/acl/' . $this->route['controller'] . '.php';
-            if ($this->isAcl('all'))
-            {
-                return true;
-            }
-            elseif (isset($_SESSION['authorize']['id']) and $this->isAcl('authorize'))
-            {
-                return true;
-            }
-            elseif (!isset($_SESSION['authorize']['id']) and $this->isAcl('guest'))
-            {
-                return true;
-            }
-            elseif (isset($_SESSION['admin']) and $this->isAcl('admin'))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public function isAcl($key)
-        {
-            return in_array($this->route['action'], $this->acl[$key]);
+            return new $path;    
         }
     }
+
+    public function checkAcl()
+    {
+        $this->acl = require 'application/acl/' . $this->route['controller'] . '.php';
+        if ($this->isAcl('all'))
+        {
+            return true;
+        }
+        elseif (isset($_SESSION['authorize']['id']) and $this->isAcl('authorize'))
+        {
+            return true;
+        }
+        elseif (!isset($_SESSION['authorize']['id']) and $this->isAcl('guest'))
+        {
+            return true;
+        }
+        elseif (isset($_SESSION['admin']) and $this->isAcl('admin'))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public function isAcl($key)
+    {
+        return in_array($this->route['action'], $this->acl[$key]);
+    }
+}
