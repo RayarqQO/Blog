90dcc3c6f7a8a6433615fbee772c49b6b6698489
Yaroslav <ray.last.light@gmail.com>
2021-10-03 00:45:41 +0300

    Update about.php

--- application/views/main/about.php
+++ application/views/main/about.php
@@ -1,19 +1,7 @@
-<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-                <div class="page-heading">
-                    <h1>Я программист</h1>
-                    <span class="subheading">записываю бесплатные обучающие видео по PHP</span>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-            <p>Здесь могло быть больше текста, но мне лень писать :)</p>
+<div class="image__section">
+    <div class="image__item" style="background-image: url('/public/images/about-bg.jpg');">
+        <div class="text__section">
+            <p>Hello, my name is Yaroslav!</p>
         </div>
     </div>
 </div>
