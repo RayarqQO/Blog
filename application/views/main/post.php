103eb913665af121248fec3690948957cae88af5
Yaroslav <ray.last.light@gmail.com>
2021-10-05 20:25:16 +0300

    Update post.php

--- application/views/main/post.php
+++ application/views/main/post.php
@@ -1,3 +1,10 @@
 <div class="image__section">
-    <div class="image__item"></div>
+    <div class="image__item" style="background-image: url('/public/materials/<?php echo $data['id'];?>.jpg');"></div>
+</div>
+<div class="page__heading">
+    <h1><?php echo $data['name'];?></h1>
+    <span><?php echo $data['description'];?></span>
+</div>
+<div class="container">
+    <?php echo $data['text']; ?>
 </div>
