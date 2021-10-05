361a18e4d18e95025d3544004e42587b3ffa3263
Yaroslav <ray.last.light@gmail.com>
2021-10-05 20:25:08 +0300

    Update index.php
    Posts output

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,15 +1,21 @@
 <div class="image__section">
     <div class="image__item"></div>
 </div>
-<div class="post__section">
-    <div class="post__preview">
-        <a href="#">Post id</a>
-        <h2 class="post__title">Post title</h2>
-        <h4 class="post__description">Post description</h4>
-    </div>
-</div>
-<?php debug($list);?>
+
+<?php if(empty($list)): ?>
+    <p>Список постов пуст!</p>
+<?php else:?>
+    <?php foreach ($list as $val): ?>
+        <div class="post__section">
+            <div class="post__preview">
+                <a href="/post/<?php echo $val['id'];?>">
+                    <h2 class="post__title"><?php echo htmlspecialchars($val['name'], ENT_QUOTES);?></h2>
+                    <h4 class="post__description"><?php echo htmlspecialchars($val['description'], ENT_QUOTES);?></h4>
+                </a>
+            </div>
+        </div>
+    <?php endforeach;?>
+<?php endif;?>
 <div class="pagination__container">
-    20:11
     <?php echo $pagination;?>
 </div>
